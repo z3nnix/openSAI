534df90365972188cc413768b3a51ef963d06fab
z3nnix <ponomarv-d@internet.ru>
2025-02-20 20:52:33 +0300

    updated filtermaker.sh

--- filtermaker.sh
+++ filtermaker.sh
@@ -2,30 +2,34 @@
 
 FILE="vocabulary.bot"
 
+# Check if the file exists
 if [ ! -f "$FILE" ]; then
-  echo "Файл '$FILE' не существует."
+  echo "The file vocabulary.bot does not exist."
   exit 1
 fi
+
+TEMP_FILE=$(mktemp)
 
 if [ -n "$1" ]; then
   SYMBOL="$1"
   
-  printf "Are you sure you want to delete the symbol '$SYMBOL'? This will remove it in ALL phrases.(Y/n) "
+  # Confirm deletion of lines containing the specified symbol(s)
+  printf "Are you sure you want to delete lines containing the symbol(s) '$SYMBOL', https://, http://, and @? (Y/n) "
   read REPLY
   
   if [ "$REPLY" != "y" ] && [ "$REPLY" != "Y" ]; then
-    echo "Canceled."
+    echo "Operation canceled."
     exit 0
   fi
 
-  TEMP_FILE=$(mktemp)
-  sed "s/$SYMBOL//g" "$FILE" | sort | uniq > "$TEMP_FILE"
+  # Remove lines containing the specified symbol(s), https://, http://, and @, and sort them
+  grep -vE "$SYMBOL|https://|http://|@" "$FILE" | sort | uniq > "$TEMP_FILE"
 else
-  TEMP_FILE=$(mktemp)
-  
-  sort "$FILE" | uniq > "$TEMP_FILE"
+  # Remove lines containing https://, http://, and @, and sort them
+  grep -vE "https://|http://|@" "$FILE" | sort | uniq > "$TEMP_FILE"
 fi
 
+# Replace the original file with the updated content
 mv "$TEMP_FILE" "$FILE"
 
 echo "Done."
